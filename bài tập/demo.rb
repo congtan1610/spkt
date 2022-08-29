1f1b8e922424cbb7bf05244659af14c332561285
congtan1610 <congtan125@gmail.com>
2022-08-29 23:38:43 +0700

    skjadhs

---
+++ bài tập/demo.rb
@@ -0,0 +1,13 @@
+puts("Xin chào nguyễn công tạn 1911505310147")
+num1 = 4
+num2 = 5
+puts("num: #{num1}")
+
+if num1 > num2
+puts("max: #{num1}")
+else
+  puts("max: #{num2}")
+end
+
+ num4 = gets
+ puts("num: #{num4}")
